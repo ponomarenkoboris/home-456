465c97a1b497556c0253668e73d1b21b4e9779c1
Boris Ponomarenko <borisponomarenko.dmi@gmail.com>
2021-06-24 17:22:15 +0300

    Изменена логика weather/Weather.tsx

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -1,14 +1,29 @@
 import React, { useState } from 'react'
 import { WEATHER_API_KEY } from '../../api/apiKeys'
+import './styles/Weather.scss'
 
-// TODO styles and render logic
-export function Weather() {    
-    const [weather, setWeather] = useState<object | null>(null)
+interface IWeather {
+    location: {
+        name: string,
+        region: string,
+        country: string
+    },
+    current: {
+        temp_c: number,
+        condition: {
+            icon: string
+        }
+    }
+}
 
+// TODO styles and render logic 
+export function Weather() {
+    const [weather, setWeather] = useState<IWeather | null>(null)
+    
     if (weather === null && 'geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition(({ coords }) => {
             const { latitude, longitude }: { latitude: number, longitude: number } = coords
-            fetch(`http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${latitude + ',' + longitude}&days=5&aqi=yes&alerts=yes`)
+            fetch(`http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${latitude + ',' + longitude}&days=5&aqi=no&alerts=yes`)
                 .then(result => result.json())
                 .then(data => {
                     setWeather(data)
@@ -16,9 +31,20 @@ export function Weather() {
         })
     }
 
-    return (
+    return weather ? (
         <div className="weather">
-            <h1>{weather === null ? 'Прогноз погоды' : JSON.stringify(weather)}</h1>
+            <div className="weather__header">
+                <p>{weather.location.name},&nbsp;</p>
+                <p>{weather.location.region},&nbsp;</p>
+                <p>{weather.location.country}</p>
+            </div>
+            <div className="weather__body">
+                <h1>{weather.current.temp_c}&nbsp;c.</h1>
+            </div>
+        </div>
+    ) : (
+        <div className="notWeather">
+            <h1>Подождите...</h1>
         </div>
     )
 }
